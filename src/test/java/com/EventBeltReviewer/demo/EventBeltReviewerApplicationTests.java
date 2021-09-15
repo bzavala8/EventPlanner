c0942705138432996a4b43b306bf1799dd56e7f8
admin <admin@admins-air>
2021-09-15 11:16:21 -0500

    Initial Commit

---
+++ src/test/java/com/EventBeltReviewer/demo/EventBeltReviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EventBeltReviewer.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventBeltReviewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
