aeaaf6d2971d367d76628d273e24cb1c8444b8c9
admin <admin@admins-air>
2021-09-16 11:36:13 -0500

    Finished dashboard functionality

--- src/main/java/com/EventBeltReviewer/demo/repositories/EventRepository.java
+++ src/main/java/com/EventBeltReviewer/demo/repositories/EventRepository.java
@@ -10,5 +10,4 @@ public interface EventRepository extends CrudRepository<Event, Long>{
 	List<Event> findAll();
 	List<Event> findByState(String state);
 	List<Event> findByNotInState(String state);
-	
 }
